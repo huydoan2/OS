06dd193f09c4d6a621f62ef2af33f1bbf16100a4
huydoan2 <huydoan2@c446b1ba-58ae-4c9c-992b-54f4acc4b95b>
2016-03-12 20:17:49 +0000

    Added one static page table

--- _projects/4Tuxedos/student-distrib/Paging.c
+++ _projects/4Tuxedos/student-distrib/Paging.c
@@ -2,15 +2,15 @@
 
 void paging_init()
 {
-	int i,j;
-	for(i = 0; i < MAX_SIZE; ++i)
+	int i;
+	int val;
+
+	for(i = 2; i < MAX_SIZE; ++i)
 	{
-		page_directory[i] = 0;
-		for(j = 0; j < MAX_SIZE; ++j)
-			page_table[j] = 0;
+		page_directory[i] = 0;		
 	}
 
-	
+
 }
 
 
