6850c5843bb3547db65053f6147257e2dd29736d
zzeng13 <zzeng13@c446b1ba-58ae-4c9c-992b-54f4acc4b95b>
2016-03-12 23:03:27 +0000

    From Ziheng||Changed pagine||not working

--- student-distrib/Paging.c
+++ student-distrib/Paging.c
@@ -34,20 +34,20 @@ void paging_init()
 	 //load page dir and enable paging
 
 	 asm volatile("                  \n\
-		    	push %ebp		   \n\
-				mov %esp, %ebp      \n\
-				mov 8(%esp), %eax	 \n\
-				mov %eax, %cr3	 \n\
-				mov %ebp, %esp	 \n\
-				pop %ebp			 \n\
+		    	push %%ebp		   \n\
+				mov %%esp, %%ebp      \n\
+				mov 8(%esp), %%eax	 \n\
+				mov %%eax, %%cr3	 \n\
+				mov %%ebp, %%esp	 \n\
+				pop %%ebp			 \n\
 
-				push %ebp			 \n\
-				mov %esp, %ebp		 \n\
-				mov %cr0, %eax		 \n\
-				or $0x80000000, %eax  \n\
-				mov %eax, %cr0		 \n\
-				mov %ebp, %esp		 \n\
-				pop %ebp			 \n\
+				push %%ebp			 \n\
+				mov %%esp, %%ebp		 \n\
+				mov %%cr0, %%eax		 \n\
+				or $0x80000000, %%eax  \n\
+				mov %%eax, %%cr0		 \n\
+				mov %%ebp, %%esp		 \n\
+				pop %%ebp			 \n\
 
 				ret 				 \n\
 		    "
