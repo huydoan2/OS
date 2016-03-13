93a0979de4eaaa329fb5fa8c550dc06758cada24
zzeng13 <zzeng13@c446b1ba-58ae-4c9c-992b-54f4acc4b95b>
2016-03-13 00:56:07 +0000

    From Ziheng||Changed pagine||not working

--- _projects/4Tuxedos/student-distrib/Paging.c
+++ _projects/4Tuxedos/student-distrib/Paging.c
@@ -34,7 +34,7 @@ void paging_init()
 	 //load page dir and enable paging
 
 	 asm volatile(
-	 		"movl page_directory, %%eax \n"
+	 		"movl %0, %%eax \n"
 	 		"movl  %%eax, %%cr3 		\n"
 			"movl  %%cr0, %%eax 		\n"
 			"orl  $0x80000000, %%eax 	\n"
@@ -43,7 +43,7 @@ void paging_init()
 			"orl  $0x00000010, %%eax 	\n"
 			"movl %%eax, %%cr4 			\n"
 		    :
-		    :
+		    : "r" (&page_directory[0])
 		    : "%eax"
 			);
 			
