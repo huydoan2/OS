1742a2c70c1070140ed0e97acdabf47ae6fbcc11
zzeng13 <zzeng13@c446b1ba-58ae-4c9c-992b-54f4acc4b95b>
2016-03-13 00:07:16 +0000

    From Ziheng||Changed pagine||not working

--- student-distrib/Paging.c
+++ student-distrib/Paging.c
@@ -30,7 +30,7 @@ void paging_init()
 	 physAddr += PT_INCREMENT;
 	 page_directory[1] = ((physAddr )| PD_ENTRY_INIT_VAL_1);
 
-    printf("pd[0]: %x ||pd[1]: %x||pt: %x\n||pt[0]: %x",  page_directory[0],page_directory[1],first_page_table, first_page_table[0]);
+    printf("pd[0]: %x ||pd[1]: %x||pt: %x||pt[1023]: %x",  page_directory[0],page_directory[1],first_page_table, first_page_table[1023]);
 	 //load page dir and enable paging
 /*
 	 asm volatile("                     \n\
