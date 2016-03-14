eb9412b3bdc6568474e4abcc326ef438521c2150
zzeng13 <zzeng13@c446b1ba-58ae-4c9c-992b-54f4acc4b95b>
2016-03-14 15:06:49 +0000

    From Ziheng||Changed terminal driver: added function prototypes and constants

---
+++ _projects/4Tuxedos/student-distrib/terminal.c
@@ -0,0 +1,49 @@
+/*terminal.c
+ * - terminal driver for text user interface
+ *
+ */
+
+ #include "terminal.h"
+
+
+void scroll_screen(void){
+
+	/*set the black color*/
+
+	/*determine whether the end of screen is reached*/
+
+		/*move the currnet video display up by a line*/
+
+		/*set blank to the last line of the video*/
+
+}
+
+//DO we even need this ???
+void blink_cursor(void){
+
+	/*obtain the current location of the cursor*/
+
+	/*send command to the CRT control register of the VGA controller
+	  to create the blinking effect*/
+
+
+}
+
+
+
+
+extern void init_videoMem(){
+
+	/*set the text-mode video memory pointer*/
+
+	/*clear the screen*/
+
+}
+
+
+extern void set_text_color(text_t text_attrib){
+	/*assign the color*/
+	//what is the purpose of this function again??
+	text_color = text_attrib;
+	return;
+}
--- _projects/4Tuxedos/student-distrib/terminal.h
+++ _projects/4Tuxedos/student-distrib/terminal.h
@@ -3,16 +3,45 @@
 
 #define MAX_ROW 80
 #define MAX_COLUMN 25
-#define VIDEO_MEM_START 0xB000
+#define VIDEO_MEM_START 0xB8000 //starting address of the video memory buffer
+#define ATTRIB_COLOR_DEFULT 0x0F00 //the default attribute color for attriute (back:black, fore:white)
+#define BLANK 0x20|ATTRIB_COLOR_DEFULT
 
+
+#include "lib.h"
+
+
+/*--------global variables and structures-------*/
+/*text structure for a single memory entry*/
 typedef struct text{
-	uint8_t back_color : 4;
-	uint8_t fore_color : 4;
-	char	character;
-} text_t __attribute((packed));
+	union{ 
+		uint16_t val;
+		struct{
+			uint8_t back_color : 4;
+			uint8_t fore_color : 4;
+			char	character;
+		} __attribute__((packed));
+	};
+} text_t;
 
-int32_t cursor;
 
+text_t text_color;
+ 
+/*variables to locate the cursor location on screen*/
+int32_t cursor_x = 0;	//initialized to be the right upper corner 
+int32_t cursor_y = 0;
+
+/*--------functions for TUI in the terminal--------*/
+/*function that scrolls the screen */
+void scroll_screen(void);
+/*function that update the position of teh cursor*/
+void update_cursor(void);
+/*function that clears the screen*/
+//extern void clear_screen(void);
+/*function that initializes the video memory*/
+extern void init_videoMem();
+/*functions that set the color of the text*/
+extern void set_text_color(text_t text_attrib);
 
 
 
