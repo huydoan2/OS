401eb475591f95c0970c915c3fffe1f5b727836c
kyang18 <kyang18@c446b1ba-58ae-4c9c-992b-54f4acc4b95b>
2016-03-14 20:52:34 +0000

    keyboard driver commnet done

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -14,10 +14,13 @@
 #define right_shift_off 0xB6
 #define caps_lock_on 0x3A
 #define caps_lock_off 0xBA
+#define end_of_press 0x81
+#define size_of_keys 128
+#define max_flag	8
 //scancode array for keyboard
 //0 = no output
 //size of the scancode is 88
-char scancode [128] = {
+char scancode [size_of_keys] = {
 	'\0','1','2','3','4','5','6','7','8','9','0','-','=','\b','\t',//backspace and tab
 	'q','w','e','r','t','y','u','i','o','p','[',']','\n',0,//Control
 	'a','s','d','f','g','h','j','k','l',';','\'','`',0,//Left shift
@@ -28,7 +31,7 @@ char scancode [128] = {
 };
 
 
-char shift_scancode[128] =
+char shift_scancode[size_of_keys] =
 {
 	'\0','!','@','#','$','%','^','&','*','(',')','_','+','\b','\t', //backspace and tab
  	'Q','W','E','R','T','Y','U','I','O','P','{','}','\n',0,//Control
@@ -40,7 +43,7 @@ char shift_scancode[128] =
   0,0,0,0/* F11 Key */,0/* F12 Key */,0,  /* All Release keys are undefined */ 
 };
 
-char caps_scancode[128] =
+char caps_scancode[size_of_keys] =
 {
 	'\0','1','2','3','4','5','6','7','8','9','0','-','=','\b','\t',//backspace and tab
 	'Q','W','E','R','T','Y','U','I','O','P','[',']','\n',0,//Control
@@ -51,7 +54,7 @@ char caps_scancode[128] =
  	0,0,0,0,0,0, /* All Release keys are undefined */
 };
 
-char caps_shift_scancode[128] =
+char caps_shift_scancode[size_of_keys] =
 {
 	'\0','!','@','#','$','%','^','&','*','(',')','_','+','\b','\t', //backspace and tab
  	'q','w','e','r','t','y','u','i','o','p','{','}','\n',0,//Control
@@ -63,8 +66,9 @@ char caps_shift_scancode[128] =
   0,0,0,0/* F11 Key */,0/* F12 Key */,0,  /* All Release keys are undefined */ 
 };
 
-int shift_flag = 0;
-int caps_lock_flag = 0;
+int shift_flag = 0;				/*flag for shift*/
+int caps_lock_flag = 0;			/*flag for caps*/
+
 /* 
  * getScancode
  *   DESCRIPTION: get a keyboard input from keyboard_data address and return the data
@@ -96,48 +100,60 @@ char getScancode()
 char getchar()
 {
 	unsigned char c = getScancode();
+
+	/*if the left shift or right shift is pressed, set the shift flag to 1*/
 	if(c == left_shift_on || c == right_shift_on)
 		shift_flag = 1;
+	/*if the left shift or right shift is released, set the shift flag to 0*/
 	else if(c == left_shift_off || c == right_shift_off)
 		shift_flag = 0;
 	
+	/*if the caps lock is pressed, change the capslock flag*/
 	if(c == caps_lock_on)
 		caps_lock_flag++;
 	else if(c == caps_lock_off)
 		caps_lock_flag = caps_lock_flag;
 	
-	if(caps_lock_flag == 8)
+	/*reset the caps lock flag once it goes over the max*/
+	if(caps_lock_flag == max_flag)
 		caps_lock_flag = 0;	
 
-	if((caps_lock_flag%2) ==0)
+	/*caps lock off case*/
+	if((caps_lock_flag % 2) ==0)
 	{
+		/*caps lock off and shift on*/
 		if(shift_flag == 1)
 		{
-	    	if(c < 0x81)
+	    	if(c < end_of_press)
 	      		return shift_scancode[c-1];
 	    	else
 	      		return shift_scancode[0];
 		}
+		/*caps lock off and shift off*/
 		else
 		{
-			if(c < 0x81)
+			if(c < end_of_press)
 	      		return scancode[c-1];
 	    	else
 	      		return scancode[0];
 		}
 	}
+
+	/*caps lock on case*/
 	else
 	{
+		/*caps lock on and shift on*/
 		if(shift_flag == 1)
 		{
-	    	if(c < 0x81)
+	    	if(c < end_of_press)
 	      		return caps_shift_scancode[c-1];
 	    	else
 	      		return caps_shift_scancode[0];
 		}
+		/*caps lock on and shift off*/
 		else
 		{
-			if(c < 0x81)
+			if(c < end_of_press)
 	      		return caps_scancode[c-1];
 	    	else
 	      		return caps_scancode[0];
@@ -158,8 +174,8 @@ char getchar()
  */
 void keyboard_init()
 {
-	shift_flag = 0;
-	caps_lock_flag = 0;
+	shift_flag = 0;					/*initialize the shift flag to zero*/
+	caps_lock_flag = 0;				/*initialize the caps lock flag to zero*/
 	enable_irq(keyboard_irq_num);
 }        
 
@@ -176,7 +192,7 @@ void keyboard_init()
  */
 void keyboard_handler()
 {	
-	if(getchar() != '\0')
+	if(getchar() != '\0')			/*if the scancode value is not empty, print out the character*/
 		printf("%c", getchar());
 	send_eoi(keyboard_irq_num);
 	asm volatile("                  \n\
