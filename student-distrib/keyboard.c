facaa9c6d2d72f22d62a1333453851d20011c14f
kyang18 <kyang18@c446b1ba-58ae-4c9c-992b-54f4acc4b95b>
2016-03-16 18:38:43 +0000

    joshua yang: simplified scancode arrays

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -30,10 +30,9 @@ char scancode [size_of_keys] = {
 	'\0','1','2','3','4','5','6','7','8','9','0','-','=','\b',0,//backspace and tab
 	'q','w','e','r','t','y','u','i','o','p','[',']','\n',0,//Control
 	'a','s','d','f','g','h','j','k','l',';','\'','`',0,//Left shift
-  '\\','z','x','c','v','b','n','m',',','.','/',0,
-  '*',0,' ',0,0,0,0,0,0,0,0,0,0,0,0,0,
-  '7',0,'9','-',0,'5',0,'+','1',0,'3','0','.',
- 	0,0,0,0,0,0, /* All Release keys are undefined */
+  	'\\','z','x','c','v','b','n','m',',','.','/',0,
+  	'*',0,' ',0,0,0,0,0,0,0,0,0,0,0,0,0,
+  	'7',0,'9','-',0,'5',0,'+','1',0,'3','0','.',0,0,0,0,0,0, /* All Release keys are undefined */
 };
 
 
@@ -43,10 +42,8 @@ char shift_scancode[size_of_keys] =
  	'Q','W','E','R','T','Y','U','I','O','P','{','}','\n',0,//Control
   'A','S','D','F','G','H','J','K','L',':','"','~',0,//Left shift
   '|','Z','X','C','V','B','N','M','<','>','?',0,//Right shift
-  '*',0,' ',0/* Caps lock */,0,0,0,0,0,0,0,0,0,0,0/* 69 - Num lock*/,0/* Scroll Lock */,
-  0/* Home key */,0/* Up Arrow */,0/* Page Up */,'-',0/* Left Arrow */,0,0/* Right Arrow */,
-  '+',0/* 79 - End key*/,0/* Down Arrow */,0/* Page Down */,0/* Insert Key */,0/* Delete Key */,
-  0,0,0,0/* F11 Key */,0/* F12 Key */,0,  /* All Release keys are undefined */ 
+  '*',0,' ',,0,0,0,0,0,0,0,0,0,0,0,0,0,
+  '7',0,'9','-',0,'5',0,'+','1',0,'3','0','.',0,0,0,0,0,0, /* All Release keys are undefined */
 };
 
 char caps_scancode[size_of_keys] =
@@ -56,8 +53,7 @@ char caps_scancode[size_of_keys] =
   'A','S','D','F','G','H','J','K','L',';','\'','`',0,//Left shift
   '\\','Z','X','C','V','B','N','M',',','.','/',0,
   '*',0,' ',0,0,0,0,0,0,0,0,0,0,0,0,0,
-  '7',0,'9','-',0,'5',0,'+','1',0,'3','0','.',
- 	0,0,0,0,0,0, /* All Release keys are undefined */
+  '7',0,'9','-',0,'5',0,'+','1',0,'3','0','.',0,0,0,0,0,0, /* All Release keys are undefined */
 };
 
 char caps_shift_scancode[size_of_keys] =
@@ -66,10 +62,8 @@ char caps_shift_scancode[size_of_keys] =
  	'q','w','e','r','t','y','u','i','o','p','{','}','\n',0,//Control
   'a','s','d','f','g','h','j','k','l',':','"','~',0,//Left shift
   '|','z','x','c','v','b','n','m','<','>','?',0,//Right shift
-  '*',0,' ',0/* Caps lock */,0,0,0,0,0,0,0,0,0,0,0/* 69 - Num lock*/,0/* Scroll Lock */,
-  0/* Home key */,0/* Up Arrow */,0/* Page Up */,'-',0/* Left Arrow */,0,0/* Right Arrow */,
-  '+',0/* 79 - End key*/,0/* Down Arrow */,0/* Page Down */,0/* Insert Key */,0/* Delete Key */,
-  0,0,0,0/* F11 Key */,0/* F12 Key */,0,  /* All Release keys are undefined */ 
+  '*',0,' ',,0,0,0,0,0,0,0,0,0,0,0,0,0,
+  '7',0,'9','-',0,'5',0,'+','1',0,'3','0','.',0,0,0,0,0,0, /* All Release keys are undefined */
 };
 
 int shift_flag;				/*flag for shift*/
