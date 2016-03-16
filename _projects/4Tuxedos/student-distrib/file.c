e88bc423dbd856ef43aa6a49872c05a89c6f4914
kyang18 <kyang18@c446b1ba-58ae-4c9c-992b-54f4acc4b95b>
2016-03-16 21:52:31 +0000

    joshua: file.c & file.h started

---
+++ _projects/4Tuxedos/student-distrib/file.c
@@ -0,0 +1,31 @@
+/*file.c 
+ * - function for initializing file and functions
+ */
+#include "file.h"
+
+
+int file_open()
+{
+	return 0;
+}
+
+int file_close()
+{
+	return 0;
+}
+
+void file_handler(void)
+{
+	
+}
+
+int file_read(int * buff, int num_bytes)
+{
+	return 0;
+}
+
+int file_write(int * buff, int num_bytes)
+{
+	return -1;
+}
+
