c9f0a3e752eb36747b9acd69316b3974c268951c
jjsando2 <jjsando2@c446b1ba-58ae-4c9c-992b-54f4acc4b95b>
2016-03-17 02:20:08 +0000

    added read dentry

--- student-distrib/file.c
+++ student-distrib/file.c
@@ -2,6 +2,15 @@
  * - function for initializing file and functions
  */
 #include "file.h"
+#include "types.h"
+#include "lib.h"
+
+
+#define max_dentries 62
+
+bootblock_t bootblock;
+dentry_t* dentry_file_system;
+inode_t* inode_file_system;
 
 
 int file_open()
@@ -29,3 +38,48 @@ int file_write(int * buff, int num_bytes)
 	return -1;
 }
 
+
+int32_t read_dentry_by_name(const uint8_t* fname, struct dentry_t* dentry)
+{
+	int i;
+
+	for(i = 0; i < max_dentries; i++)
+	{
+
+		//if(condition is true)
+		// {
+		// 	strcpy(dentry->filename, dentry_file_system[index].filename);
+		// 	dentry->file_type = dentry_file_system[index].file_type;
+		// 	dentry->inode_num = dentry_file_system[index].inode_num;
+
+		// 	return 0;
+		// }
+	}
+
+	return -1;
+}
+
+
+int32_t read_dentry_by_index(uint32_t index, struct dentry_t* dentry)
+{
+	//check if index is valid
+	if(index > max_dentries)
+	{
+		return -1;
+	}
+
+	strcpy(dentry->filename, dentry_file_system[index].filename);
+	dentry->file_type = dentry_file_system[index].file_type;
+	dentry->inode_num = dentry_file_system[index].inode_num;
+
+	return 0;
+}
+
+int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length)
+{
+
+	if(inode >= bootblock.num_inodes || offset >= inode_file_system[inode].length_in_B)
+		return -1;
+
+	return 0;
+}
