a8b7b95e513e3b79217378abd32513165d0c6f7e
jjsando2 <jjsando2@c446b1ba-58ae-4c9c-992b-54f4acc4b95b>
2016-03-17 02:20:08 +0000

    added read dentry

--- _projects/4Tuxedos/student-distrib/file.h
+++ _projects/4Tuxedos/student-distrib/file.h
@@ -4,6 +4,8 @@
 #ifndef FILE_H
 #define FILE_H
 
+#include "types.h"
+
 //handler for file
 extern void file_handler(void);
 
@@ -14,9 +16,43 @@ extern int file_open();
 extern int file_close();
 
 //read function for file
-extern int file_read(int * buff, int num_bytes);
+extern int file_read(int* buff, int num_bytes);
 
 //write function for file
-extern int file_write(int * buff, int num_bytes);
+extern int file_write(int* buff, int num_bytes);
+
+//bootblock starting address
+/*
+printf("Module %d loaded at address: 0x%#x\n", mod_count, (unsigned int)mod->mod_start);
+printf("Module %d ends at address: 0x%#x\n", mod_count, (unsigned int)mod->mod_end);
+*/
+
+typedef struct bootblock_t{
+	int32_t num_dentries;
+	int32_t num_inodes;
+	int32_t num_dblocks;
+	int32_t reserved[13];
+	//need dir entries here???
+} bootblock_t;
+
+
+typedef struct dentry_t{
+	int8_t filename[32];
+	int32_t file_type;
+	int32_t inode_num;
+	int32_t reserved[6];
+} dentry_t;
+
+typedef struct inode_t{
+	int8_t length_in_B;
+	int32_t data_block[]; //how many blocks?
+} inode_t;
+
+
+int32_t read_dentry_by_name(const uint8_t* fname, struct dentry_t* dentry);
+
+int32_t read_dentry_by_index(uint32_t index, struct dentry_t* dentry);
+
+int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
 
 #endif 
