7a67d7a3775e739b22e2e1e6afd221ec51fe0b44
zzeng13 <zzeng13@c446b1ba-58ae-4c9c-992b-54f4acc4b95b>
2016-03-18 23:25:51 +0000

    working read_dara and parsing?? testing in progress

--- _projects/4Tuxedos/student-distrib/kernel.c
+++ _projects/4Tuxedos/student-distrib/kernel.c
@@ -195,7 +195,7 @@ entry (unsigned long magic, unsigned long addr)
 	/* Enable interrupts */
 	/* Do not enable the following until after you have set up your
 	 * IDT correctly otherwise QEMU will triple fault and simple close
-	 * without showing you any output *1
+	 * without showing you any output */
 	printf("Enabling Interrupts\n");
 
 
