b64c88a45eaad51a8ec35d584d6a66c8b3c4c5aa
zzeng13 <zzeng13@c446b1ba-58ae-4c9c-992b-54f4acc4b95b>
2016-03-19 23:54:28 +0000

    even more comments added

--- student-distrib/file_desc.c
+++ student-distrib/file_desc.c
@@ -11,8 +11,17 @@
 
 /*PCB array*/
 file_decs_t PCB[PCB_SIZE];
-
-
+/* 
+ * init_PCB
+ *   DESCRIPTION: initialize the array of the PCBs
+ *-----------------------------------------------------------------------------------
+ *   INPUTS: none
+ *   OUTPUTS: none
+ *   RETURN VALUE: none
+ *-----------------------------------------------------------------------------------
+ *   SIDE EFFECTS: every PCB now has the flag indicates not in use
+ *
+ */
 void init_PCB(){
 	int i;
 	/*initialize file position and flag*/
@@ -22,6 +31,17 @@ void init_PCB(){
 	}
 }
 
+/* 
+ * check_avail
+ *   DESCRIPTION: find the first availible PCB block in the array 
+ *-----------------------------------------------------------------------------------
+ *   INPUTS: none
+ *   OUTPUTS: none
+ *   RETURN VALUE: the index of the first availible PCB block
+ *-----------------------------------------------------------------------------------
+ *   SIDE EFFECTS: none
+ *
+ */
 int check_avail(){
 	int i;
 	/*traverse through the PCB size and for the first empty*/
@@ -36,18 +56,22 @@ int check_avail(){
 	return -1;
 }
 
+/* 
+ * open
+ *   DESCRIPTION: create a new PCB, call the specific open function
+ *-----------------------------------------------------------------------------------
+ *   INPUTS: - filename: the name of the file to be opened
+ *   OUTPUTS: none
+ *   RETURN VALUE: the index of PCB block that stores the newly opened fd (fd value)
+ *-----------------------------------------------------------------------------------
+ *   SIDE EFFECTS: none
+ *
+ */
 int32_t open(const uint8_t* filename)
 {
 	dentry_t dentry;
 	file_decs_t fd;
 	int PCB_idx;
-
-	/*declare function pointers*/
-	// systemcallFunc_ptr open_ptr;
-	// systemcallFunc_ptr close_ptr;
-	// systemcallFunc_ptr read_ptr;
-	// systemcallFunc_ptr write_ptr;
-
 	/*check if the PCB array has empty spaces*/
 	if ((PCB_idx = check_avail()) == -1){
 		return -1;
@@ -64,10 +88,6 @@ int32_t open(const uint8_t* filename)
 			fd.fops.close_ptr = &rtc_close;
 			fd.fops.read_ptr = &rtc_read;
 			fd.fops.write_ptr = &rtc_write;
-			// fd.fops[OPEN] = open_ptr;
-			// fd.fops[CLOSE] = close_ptr;
-			// fd.fops[READ] = read_ptr;
-			// fd.fops[WRITE] = write_ptr;
 		    break;
 
 	    case DIR:
@@ -75,10 +95,6 @@ int32_t open(const uint8_t* filename)
 			fd.fops.close_ptr = &dir_close;
 			fd.fops.read_ptr = &dir_read;
 			fd.fops.write_ptr = &dir_write;
-			// fd.fops[OPEN] = open_ptr;
-			// fd.fops[CLOSE] = close_ptr;
-			// fd.fops[READ] = read_ptr;
-			// fd.fops[WRITE] = write_ptr;
 			break;
  
 	    case REGULAR: 
@@ -86,10 +102,6 @@ int32_t open(const uint8_t* filename)
 			fd.fops.close_ptr = &file_close;
 			fd.fops.read_ptr = &file_read;
 			fd.fops.write_ptr = &file_write;
-			// fd.fops[OPEN] = open_ptr;
-			// fd.fops[CLOSE] = close_ptr;
-			// fd.fops[READ] = read_ptr;
-			// fd.fops[WRITE] = write_ptr;	
 			break;
 
 		default: 
@@ -111,11 +123,23 @@ int32_t open(const uint8_t* filename)
 
 	//return the index of the fd
 	fd.fops.open_ptr();
+
 	return PCB_idx;
 
 	
 }
-
+/* 
+ * open
+ *   DESCRIPTION: call the read function associated with the give fd
+ *-----------------------------------------------------------------------------------
+ *   INPUTS: - fd: index of the PCB array
+ *			 - nbytes: number of bytes to read from the file
+ *   OUTPUTS: - buf: the buffer that holds the data read from the file
+ *   RETURN VALUE: number of bytes read
+ *-----------------------------------------------------------------------------------
+ *   SIDE EFFECTS: none
+ *
+ */
 int32_t read(int32_t fd, void * buf, int32_t nbytes)
 {
 	uint32_t offset = 0;
@@ -129,6 +153,19 @@ int32_t read(int32_t fd, void * buf, int32_t nbytes)
 
 	return ret_val;
 }
+/* 
+ * write
+ *   DESCRIPTION: call the write function associated with the give fd
+ *-----------------------------------------------------------------------------------
+ *   INPUTS: - fd: index of the PCB array
+ *			 - nbytes: number of bytes to read from the file
+ *			 - buf: extra parameter, may be unused, depending on the type of the file
+ *   OUTPUTS: 
+ *   RETURN VALUE: 0 for successed and -1 for failed
+ *-----------------------------------------------------------------------------------
+ *   SIDE EFFECTS: none
+ *
+ */
 
 int32_t write(int32_t fd, const void * buf, int32_t nbytes)
 {
@@ -140,6 +177,19 @@ int32_t write(int32_t fd, const void * buf, int32_t nbytes)
 	
 
 }
+
+/* 
+ * close
+ *   DESCRIPTION: close a file, remove the PCB from the array (by changing the flag)
+ *-----------------------------------------------------------------------------------
+ *   INPUTS: - fd: index of the PCB array
+
+ *   OUTPUTS: 
+ *   RETURN VALUE: 0 for successed and -1 for failed
+ *-----------------------------------------------------------------------------------
+ *   SIDE EFFECTS: the PCB with the index 'fd' is unoccupied now
+ *
+ */
 
 int32_t close(int32_t fd)
 {
