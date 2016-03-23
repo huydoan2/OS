a4e5299d2391fff013df82ea2f59b2970c62c6b4
huydoan2 <huydoan2@c446b1ba-58ae-4c9c-992b-54f4acc4b95b>
2016-03-23 03:48:11 +0000

    new code for syscalls but not functioning yet

--- _projects/4Tuxedos/student-distrib/systemCalls.c
+++ _projects/4Tuxedos/student-distrib/systemCalls.c
@@ -1,63 +1,63 @@
-/* systemCalls.c
- * - definition of system call functions 
- */
-#include "systemCalls.h"
-#include "file_desc.h" //file that have sys
+// /* systemCalls.c
+//  * - definition of system call functions 
+//  */
+// #include "systemCalls.h"
+// #include "file_desc.h" //file that have sys
 
 
-  /*system call 1: halt function*/
- int32_t halt(uint8_t status){
- 	return 0;
- }
+//   /*system call 1: halt function*/
+//  int32_t halt(uint8_t status){
+//  	return 0;
+//  }
 
-  /*system call 2: execute function*/
- int32_t execute(const uint8_t* command){
- 	return 0;
+//   /*system call 2: execute function*/
+//  int32_t execute(const uint8_t* command){
+//  	return 0;
 
- }
+//  }
 
-  /*system call 3: read function*/
- int32_t read(int32_t fd, void* buf, int32_t nbytes){
- 	return read_fd(fd, buf, nbytes);
+//   /*system call 3: read function*/
+//  int32_t read(int32_t fd, void* buf, int32_t nbytes){
+//  	return read_fd(fd, buf, nbytes);
 
- }
+//  }
 
-  /*system call 4: write function*/
- int32_t write(int32_t fd, const void* buf, int32_t nbytes){
- 	return write_fd(fd, buf, nbytes);
- }
+//   /*system call 4: write function*/
+//  int32_t write(int32_t fd, const void* buf, int32_t nbytes){
+//  	return write_fd(fd, buf, nbytes);
+//  }
 
-  /*system call 5: open function*/
- int32_t open(const uint8_t* filename){
- 	return open_fd(filename);
- }
+//   system call 5: open function
+//  int32_t open(const uint8_t* filename){
+//  	return open_fd(filename);
+//  }
 
-  /*system call 6: close*/
- int32_t close(int32_t fd){
- 	return close_fd(fd);
+//   /*system call 6: close*/
+//  int32_t close(int32_t fd){
+//  	return close_fd(fd);
 
- }
+//  }
 
-  /*system call 7: getargs function*/
- int32_t getargs(uint8_t* buf, int32_t nbytes){
- 	return 0;
+//   /*system call 7: getargs function*/
+//  int32_t getargs(uint8_t* buf, int32_t nbytes){
+//  	return 0;
 
- }
+//  }
 
-  /*system call 8: vidmap function*/
- int32_t vidmap(uint8_t** screen_start){
- 	return 0;
+//   /*system call 8: vidmap function*/
+//  int32_t vidmap(uint8_t** screen_start){
+//  	return 0;
 
- }
+//  }
 
-  /*system call 9: set_handler function*/
- int32_t set_handler(int32_t signum, void* handler_address){
- 	return 0;
+//   /*system call 9: set_handler function*/
+//  int32_t set_handler(int32_t signum, void* handler_address){
+//  	return 0;
 
- }
+//  }
 
-  /*system call 10: sigreturn function*/
- int32_t sigreturn(){
- 	return 0;
+//   /*system call 10: sigreturn function*/
+//  int32_t sigreturn(){
+//  	return 0;
 
- }
+//  }
--- _projects/4Tuxedos/student-distrib/systemCalls.h
+++ _projects/4Tuxedos/student-distrib/systemCalls.h
@@ -3,32 +3,36 @@
  */
 #include "types.h"
 
+#ifndef ASM
   /*system call 1: halt function*/
- extern int32_t halt(uint8_t status);
+extern int32_t halt(uint8_t status);
 
   /*system call 2: execute function*/
- extern int32_t execute(const uint8_t* command);
+extern int32_t execute(const uint8_t* command);
 
   /*system call 3: read function*/
- extern int32_t read(int32_t fd, void* buf, int32_t nbytes);
+extern int32_t read(int32_t fd, void* buf, int32_t nbytes);
 
   /*system call 4: write function*/
- extern int32_t write(int32_t fd, const void* buf, int32_t nbytes);
+extern int32_t write(int32_t fd, const void* buf, int32_t nbytes);
 
   /*system call 5: open function*/
- extern int32_t open(const uint8_t* filename);
+extern int32_t open(const uint8_t* filename);
 
   /*system call 6: close*/
- extern int32_t close(int32_t fd);
+extern int32_t close(int32_t fd);
 
   /*system call 7: getargs function*/
- extern int32_t getargs(uint8_t* buf, int32_t nbytes);
+extern int32_t getargs(uint8_t* buf, int32_t nbytes);
 
   /*system call 8: vidmap function*/
- extern int32_t vidmap(uint8_t** screen_start);
+extern int32_t vidmap(uint8_t** screen_start);
 
   /*system call 9: set_handler function*/
- extern int32_t set_handler(int32_t signum, void* handler_address);
+extern int32_t set_handler(int32_t signum, void* handler_address);
 
   /*system call 10: sigreturn function*/
- extern int32_t sigreturn();
+extern int32_t sigreturn();
+
+#endif /* ASM */
+
