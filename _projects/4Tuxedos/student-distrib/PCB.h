d388804c01207ada07504461103e10f1d1e59ebd
huydoan2 <huydoan2@c446b1ba-58ae-4c9c-992b-54f4acc4b95b>
2016-04-01 21:01:05 +0000

    Added PCB modules

---
+++ _projects/4Tuxedos/student-distrib/PCB.h
@@ -0,0 +1,29 @@
+#ifndef PCB_H
+#define PCH_H
+
+#include "types.h"
+#include "file_desc.h"
+
+#define NEV_8KB 0xFFFFE000
+
+
+typedef struct parent_info{
+	uint32_t pid;
+	uint32_t file_desc_t* fd_array;
+} parent_info_t;
+
+typedef struct pcb_struct{
+	uint32_t pid;
+	uint32_t esp;
+	uint32_t ebp;
+	uint32_t eip;
+	file_desc_t* fd_array;
+	parent_info parent;
+}pcb_struct_t;
+
+
+void init_PCB(pcb_struct_t* pcb, uint32_t pid, uint32_t esp, uint32_t ebp, uint32_t eip, const parent_info_t parent);
+
+
+
+#endif
