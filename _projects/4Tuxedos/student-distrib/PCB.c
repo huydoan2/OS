df3e5007dd77bcd5b786cae6414aee109407211d
jjsando2 <jjsando2@c446b1ba-58ae-4c9c-992b-54f4acc4b95b>
2016-04-03 18:51:32 +0000

    compiles now

--- _projects/4Tuxedos/student-distrib/PCB.c
+++ _projects/4Tuxedos/student-distrib/PCB.c
@@ -1,15 +1,15 @@
 #include "PCB.h"
 
-// void init_PCB(pcb_struct_t* pcb, uint32_t pid, uint32_t esp, uint32_t ebp, uint32_t eip, const parent_info_t parent )
-// {
-// 	pcb->pid = pid;
-// 	pcb->esp = esp;
-// 	pcb->ebp = ebp;
-// 	pcb->eip = eip;
-// 	pcb->fd_array = esp & NEV_8KB;	//fd_array address = stack - 8KB
-// 	init_FD(pcb->fd_array);
-// 	pcb->parent.pid = parent.pid;
-// 	pcb->parent.fd_array = parent.fd_array;
-// }
+void init_PCB(pcb_struct_t* pcb, uint32_t pid, uint32_t esp, uint32_t ebp, uint32_t eip, const parent_info_t parent )
+{
+	pcb->pid = pid;
+	pcb->esp = esp;
+	pcb->ebp = ebp;
+	pcb->eip = eip;
+	pcb->fd_array = esp & NEV_8KB;	//fd_array address = stack - 8KB
+	init_FD(pcb->fd_array);
+	pcb->parent.pid = parent.pid;
+	pcb->parent.fd_array = parent.fd_array;
+}
 
 
