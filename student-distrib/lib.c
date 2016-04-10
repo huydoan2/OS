da24c79e9e8a2d341bf2178612bdc009b2399598
kyang18 <kyang18@c446b1ba-58ae-4c9c-992b-54f4acc4b95b>
2016-04-10 21:57:39 +0000

    lib.c updated

--- student-distrib/lib.c
+++ student-distrib/lib.c
@@ -329,19 +329,24 @@ putc(uint8_t c)
     if(c == '\n' || c == '\r') {
         screen_y++;
         screen_x=0;
-    } 
+		if (screen_y > NUM_ROWS - 1)
+		{
+			scroll_screen();
+			--screen_y;
+		}
+    }
     else {
+		if (screen_y > NUM_ROWS - 1)
+		{
+			scroll_screen();
+			--screen_y;
+		}
         *(uint8_t *)(video_mem + ((NUM_COLS*screen_y + screen_x) << 1)) = c;
         *(uint8_t *)(video_mem + ((NUM_COLS*screen_y + screen_x) << 1) + 1) = ATTRIB;
         screen_x++;
 	    screen_y = (screen_y + (screen_x / NUM_COLS));
 	    screen_x %= NUM_COLS;
     }
-    if (screen_y > NUM_ROWS - 1)
-	{
-		scroll_screen();
-		--screen_y;
-	}
     cursor_update(screen_x, screen_y);
 }
 /*
@@ -746,25 +751,26 @@ display_c(uint8_t c)
 	if(c == '\n' || c == '\r') {
         screen_y++;
         screen_x=0;
-    }
-    else
-    {
-	 	*(uint8_t *)(video_mem + ((NUM_COLS*screen_y + screen_x) << 1)) = c;
-	    *(uint8_t *)(video_mem + ((NUM_COLS*screen_y + screen_x) << 1) + 1) = ATTRIB;
-	    
-	    //update screen x and screen y
-	    screen_x++;
+
+		if (screen_y > NUM_ROWS - 1)
+		{
+			scroll_screen();
+			--screen_y;
+		}
+    } 
+    else {
+    	
+		if (screen_y > NUM_ROWS - 1)
+		{
+			scroll_screen();
+			--screen_y;
+		}
+        *(uint8_t *)(video_mem + ((NUM_COLS*screen_y + screen_x) << 1)) = c;
+        *(uint8_t *)(video_mem + ((NUM_COLS*screen_y + screen_x) << 1) + 1) = ATTRIB;
+        screen_x++;
 	    screen_y = (screen_y + (screen_x / NUM_COLS));
 	    screen_x %= NUM_COLS;
-	}
-    //if the screen_y goes over the max y, scroll the screen
- 	if (screen_y > NUM_ROWS - 1)
-	{
-		scroll_screen();
-		--screen_y;
-	}
-
-	//update the cursor location
+    }
     cursor_update(screen_x, screen_y);
 }
 
