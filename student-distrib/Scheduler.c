6673742329fd1c4e0b2c4702df9589b23dd92714
zzeng13 <zzeng13@c446b1ba-58ae-4c9c-992b-54f4acc4b95b>
2016-04-19 01:24:33 +0000

    simple keyboard writing works

--- student-distrib/Scheduler.c
+++ student-distrib/Scheduler.c
@@ -24,12 +24,11 @@ void pit_handler()
 {
 	cli(); //WHERE IS THE CORESP STI FOR THIS?
 	send_eoi(pit_irq_num);
-    curr_pid = current_pid[active_pid_index];
-    active_pid_index+=1;
-    active_pid_index%=3;
+    curr_pid =find_next_pid();
+
     next_pid = current_pid[active_pid_index];//find_next_pid();//after this, active_pid_index is updated
-	process_switch_mem_map(next_pid, active_pid_index, current_terminal); 
-    //switch_task(curr_pid, next_pid);
+	//process_switch_mem_map(next_pid, active_pid_index, current_terminal); 
+    switch_task(curr_pid, next_pid);
     sti();
 }
 
@@ -44,16 +43,16 @@ void switch_task(uint32_t curr_pid,uint32_t next_pid)
     uint32_t ebp = 0;
 
     /*get esp/ebp, and update those of the current process*/
-    // asm volatile("movl %%esp, %0"
-    //                  :"=c"(esp)
-    //                  :
-    //                  :"%esp"
-    //                  );
-    // asm volatile("movl %%ebp, %0" 
-    //                  :"=c"(ebp)
-    //                  :
-    //                  :"%ebp"
-    //                  );
+    asm volatile("movl %%esp, %0"
+                     :"=c"(esp)
+                     :
+                     :"%esp"
+                     );
+    asm volatile("movl %%ebp, %0" 
+                     :"=c"(ebp)
+                     :
+                     :"%ebp"
+                     );
 
     current_pcb = find_PCB(curr_pid);
     current_pcb->esp = esp;
@@ -62,26 +61,26 @@ void switch_task(uint32_t curr_pid,uint32_t next_pid)
    
 
     /*set tss registers*/
-    // tss.ss0 =  KERNEL_DS;
-    // tss.esp0 = EIGHT_MB - tss_offset - EIGHT_KB * (next_pid); 
+    tss.ss0 =  KERNEL_DS;
+    tss.esp0 = EIGHT_MB - tss_offset - EIGHT_KB * (next_pid); 
 
     /*update esp and ebp of the next process*/
     next_pcb = find_PCB(next_pid);
     esp = next_pcb->esp;
     ebp = next_pcb->ebp;
 
-    // asm volatile("movl %0, %%esp"
-    //                  :
-    //                  :"c"(esp)
-    //                  :"%esp"
-    //                  );
-    // asm volatile("movl %0, %%ebp"
-    //                  :
-    //                  :"c"(ebp)
-    //                  :"%ebp"
-    //                  );
+    asm volatile("movl %0, %%esp"
+                     :
+                     :"c"(esp)
+                     :"%esp"
+                     );
+    asm volatile("movl %0, %%ebp"
+                     :
+                     :"c"(ebp)
+                     :"%ebp"
+                     );
     /*swtich the program image and video memory mapping */
-    //process_switch_mem_map(next_pid, active_pid_index, current_terminal); 
+    process_switch_mem_map(next_pid, active_pid_index, current_terminal); 
 
 }
 
