39798df0dcd7897499784ef83f23adc784d55fc3
zzeng13 <zzeng13@c446b1ba-58ae-4c9c-992b-54f4acc4b95b>
2016-04-19 21:10:30 +0000

    changed process info array assignment

--- student-distrib/Scheduler.h
+++ student-distrib/Scheduler.h
@@ -4,7 +4,7 @@
 #include "types.h"
 #include "PCB.h"
 
-void pit_handler(process_info_t * registers);
-void switch_task(uint32_t curr_pid,uint32_t next_pid, process_info_t * registers);
+void pit_handler();
+void switch_task(uint32_t curr_pid,uint32_t next_pid);
 #endif
 
