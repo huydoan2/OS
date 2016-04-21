deb86ce6861252b1e696117a60b738dea1fc0a18
kyang18 <kyang18@c446b1ba-58ae-4c9c-992b-54f4acc4b95b>
2016-04-21 22:22:14 +0000

    not working still

--- student-distrib/lib.c
+++ student-distrib/lib.c
@@ -325,12 +325,12 @@ void
 putc(uint8_t c)
 {
     if(c == '\n' || c == '\r') {
-        cursor_t[0][1]++;
-        cursor_t[0][0]=0;
-		if (cursor_t[0][1] > NUM_ROWS - 1)
+        cursor_t[cursor_terminal][1]++;
+        cursor_t[cursor_terminal][0]=0;
+		if (cursor_t[cursor_terminal][1] > NUM_ROWS - 1)
 		{
 			scroll_screen();
-			--cursor_t[0][1];
+			--cursor_t[cursor_terminal][1];
 		}
     }
     else {
@@ -345,7 +345,7 @@ putc(uint8_t c)
 	    cursor_t[cursor_terminal][1] = (cursor_t[cursor_terminal][1] + (cursor_t[cursor_terminal][0] / NUM_COLS));
 	    cursor_t[cursor_terminal][0] %= NUM_COLS;
     }
-    cursor_update(cursor_t[cursor_terminal][0], cursor_t[cursor_terminal][1]);
+    //cursor_update(cursor_t[cursor_terminal][0], cursor_t[cursor_terminal][1]);
 }
 /*
 * int8_t* itoa(uint32_t value, int8_t* buf, int32_t radix);
