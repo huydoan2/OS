bf2db736f5e7292417f7abe879c9fd482c7b8878
huydoan2 <huydoan2@c446b1ba-58ae-4c9c-992b-54f4acc4b95b>
2016-04-27 00:02:44 +0000

    Idea for SIGNAL

--- student-distrib/signal.c
+++ student-distrib/signal.c
@@ -1,1 +1,29 @@
 /*signal.c*/
+
+#include "signal.h"
+
+uint32_t do_signal();
+{
+	//have to do masking
+
+	setup_frame();
+	switch(regs->eax)
+	{
+		case 0x
+	}
+}
+
+uint32_t setup_frame()
+{
+	asm volatile
+	(
+		start:
+		movl $10, %eax
+		INT x80
+		end:
+		leal start, variable_start
+		leal end, variable_end
+	)
+	size = variable_end - variable_start;
+	memcpy(esp - size, variable_start, size);
+}
